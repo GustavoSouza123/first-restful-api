901e0406c5fcad6a78ec8da413656c2a8f160f19
Gustavo Souza <gustavoelia7@gmail.com>
2024-06-24 18:43:34 -0300

    api endpoints and json middleware

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
 
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large',
+    });
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({ message: 'We need a logo!' });
+    }
+
+    res.send({
+        tshirt: `👕 with your ${logo} and id of ${id}`,
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
